87b40a883ef07212d7e89e4e7d6f5f44c157a98a
BelaSchrammDev <belaschramm@aol.de>
2024-07-21 13:58:35 +0200

    add channellist to workspacemenu

--- src/app/workspacemenu/channellist/channellist.component.ts
+++ src/app/workspacemenu/channellist/channellist.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './channellist.component.scss'
 })
 export class ChannellistComponent {
+  public listdropdown = true;
+
+  toggleListDropdown() {
+    console.log('Toggle list dropdown');
+    this.listdropdown = !this.listdropdown;
+  }
+
+  addChannel(event: Event) {
+    event.stopPropagation();
+    console.log('Add channel');
+  }
 
 }
