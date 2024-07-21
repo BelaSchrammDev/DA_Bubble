1d59c79a1efbffdbf162f0834cfb836f5c49f7f4
BelaSchrammDev <belaschramm@aol.de>
2024-07-21 13:31:02 +0200

    add workspacemenu userlist

--- src/app/workspacemenu/userlist/userlist.component.ts
+++ src/app/workspacemenu/userlist/userlist.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '../../shared/service/users.service';
+import { UserbadgeComponent } from '../../users/userbadge/userbadge.component';
 
 @Component({
   selector: 'app-userlist',
   standalone: true,
-  imports: [],
+  imports: [
+    UserbadgeComponent
+  ],
   templateUrl: './userlist.component.html',
   styleUrl: './userlist.component.scss'
 })
 export class UserlistComponent {
+  public listdropdown = true;
+  public userservice = inject(UsersService);
+
+  toggleListDropdown() {
+    this.listdropdown = !this.listdropdown;
+  }
 
 }
